b82e1cf9bd391923c675db6b47af4480fae22934
adji <adjipratama22@gmail.com>
2018-09-18 13:39:27 +0700

    testpull

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!sss</h1>
+        <h1>Congratulations!TestPull</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
